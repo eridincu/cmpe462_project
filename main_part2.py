c1d5e7c624b5ba59f3f29c8abbbe3937fd87bc6f
eridincu <m.erdincoguz@gmail.com>
2021-05-17 22:29:53 +0300

    Final version of main_part2.py

--- main_part2.py
+++ main_part2.py
@@ -30,39 +30,18 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.utils import validation
 
-training_data = []
-validation_data = []
-
-accuracy_dict_MNB_COUNT = {}
-accuracy_dict_MNB_TFIDF = {}
-accuracy_dict_GNB_COUNT = {}
-accuracy_dict_GNB_TFIDF = {}
-accuracy_dict_LR_COUNT = {}
-accuracy_dict_LR_TFIDF = {}
-accuracy_dict_RFR_COUNT = {}
-accuracy_dict_RFR_TFIDF = {}
-
-accuracy = 0
-precision = 0
-recall = 0
-
-macro_avg = 0
-
-
-X_train = []
-y_train = []
-X_val = []
-y_val = []
-
-WNL = WordNetLemmatizer()
+accuracy_dict_MNB = {}
+accuracy_dict_GNB = {}
+accuracy_dict_LR = {}
+accuracy_dict_RFR = {}
 
 class Data():
     def __init__(self, header, content, rating):
         self.header = header
         self.content = content
         self.rating = rating
-
 
 def nltk_tag_to_wordnet_tag(nltk_tag):
     if nltk_tag.startswith('J'):
@@ -160,54 +139,27 @@ def read_from_txt(name):
         _list.append(element.strip())
     return _list
 
-def apply_models(key, processed_training_count, processed_validation_count, processed_training_tfidf, processed_validation_tfidf):
+def apply_models(y_train, y_val, key, processed_training_count, processed_validation_count, processed_training_tfidf, processed_validation_tfidf):
     print('Start applying models for', key, '...')
     start = time.time()
 
-    apply_rfr(key, processed_training_count, processed_validation_count, 100)
-    apply_rfr(key, processed_training_tfidf, processed_validation_tfidf, 100)
+    apply_rfr(y_train, y_val, 'count ' + key, processed_training_count, processed_validation_count, 100)
+    apply_rfr(y_train, y_val, 'tfidf ' + key, processed_training_tfidf, processed_validation_tfidf, 100)
     
-    apply_mnb(key, processed_training_count, processed_validation_count)
-    apply_mnb(key, processed_training_tfidf, processed_validation_tfidf)
+    apply_mnb(y_train, y_val, 'count ' + key, processed_training_count, processed_validation_count)
+    apply_mnb(y_train, y_val, 'tfidf ' + key, processed_training_tfidf, processed_validation_tfidf)
 
-    apply_gnb(key, processed_training_count, processed_validation_count)
-    apply_gnb(key, processed_training_tfidf, processed_validation_tfidf)
+    apply_gnb(y_train, y_val, 'count ' + key, processed_training_count, processed_validation_count)
+    apply_gnb(y_train, y_val, 'tfidf ' + key, processed_training_tfidf, processed_validation_tfidf)
 
-    apply_lr_cross_val(key, processed_training_count, processed_validation_count)
-    apply_lr_cross_val(key, processed_training_tfidf, processed_validation_tfidf)
-
-    sorted_acc_lr = sorted(
-        accuracy_dict_LR_COUNT.items(), key=lambda x: x[1], reverse=True)
-    write_results([sorted_acc_lr], ['result_accuracy_dict_LR'])
+    apply_lr_cross_val(y_train, y_val, 'count ' + key, processed_training_count, processed_validation_count)
+    apply_lr_cross_val(y_train, y_val, 'tfidf ' + key, processed_training_tfidf, processed_validation_tfidf)
 
     print('Done, exec time:', time.time() - start)
-    # sort results and write them to files
-    accuracy_dict_GNB_TFIDF = sorted(
-        accuracy_dict_GNB_TFIDF.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_GNB_COUNT = sorted(
-        accuracy_dict_GNB_COUNT.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_MNB_TFIDF = sorted(
-        accuracy_dict_MNB_TFIDF.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_MNB_COUNT = sorted(
-        accuracy_dict_MNB_COUNT.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_LR_TFIDF = sorted(
-        accuracy_dict_LR_TFIDF.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_LR_COUNT = sorted(
-        accuracy_dict_LR_COUNT.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_RFR_TFIDF = sorted(
-        accuracy_dict_RFR_TFIDF.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_RFR_COUNT = sorted(
-        accuracy_dict_RFR_COUNT.items(), key=lambda x: x[1], reverse=True)
 
-    accuracy_dict_list = [accuracy_dict_GNB_TFIDF, accuracy_dict_GNB_COUNT, accuracy_dict_MNB_TFIDF,
-         accuracy_dict_MNB_COUNT, accuracy_dict_LR_TFIDF, accuracy_dict_LR_COUNT, accuracy_dict_RFR_TFIDF, accuracy_dict_RFR_COUNT]
-    accuracy_dict_list_names = ['result_accuracy_dict_GNB_TFIDF', 'result_accuracy_dict_GNB_COUNT', 'result_accuracy_dict_MNB_TFIDF',
-               'result_accuracy_dict_MNB_COUNT', 'result_accuracy_dict_LR_TFIDF', 'result_accuracy_dict_LR_COUNT', 'result_accuracy_dict_RFR_TFIDF', 'result_accuracy_dict_RFR_COUNT']
+def apply_rfr(y_train, y_val, key, processed_training_count, processed_validation_count, n_estimator):
+    global accuracy_dict_RFR
 
-    write_results(accuracy_dict_list, accuracy_dict_list_names)
-    print()
-
-def apply_rfr(key, processed_training_count, processed_validation_count, n_estimator):
     print('Applying RFR to', key, '...')
     y_train_rfr = np.copy(y_train)
     y_val_rfr = np.copy(y_val)
@@ -225,31 +177,39 @@ def apply_rfr(key, processed_training_count, processed_validation_count, n_estim
     RFR = RandomForestRegressor(n_estimators=n_estimator, n_jobs=-1)
     RFR.fit(processed_training_count, y_train_rfr)
     score = RFR.score(processed_validation_count, y_val_rfr)
-    accuracy_dict_RFR_COUNT[str(n_estimator) + '_' + key] = score
+    accuracy_dict_RFR[str(n_estimator) + '_' + key] = score
 
 # apply logistic regression with 5-fold cross validation and save the accuracy in logistic regression result dictionary
-def apply_lr_cross_val(key, training_data, validation_data):
+def apply_lr_cross_val(y_train, y_val, key, training_data, validation_data):
+    global accuracy_dict_LR
+
     print('Applying LRC to', key, '...')
     LR = LogisticRegressionCV(cv=5, max_iter=100000, n_jobs=-1)
     LR.fit(training_data, y_train)
 
     score = LR.score(validation_data, y_val)
-    accuracy_dict_LR_COUNT[key] = score
+    accuracy_dict_LR[key] = score
+
 # apply logistic regression with gaussian naive bayes and save the accuracy in logistic regression result dictionary
-def apply_gnb(key, training_data, validation_data):
+def apply_gnb(y_train, y_val, key, training_data, validation_data):
+    global accuracy_dict_GNB
+
     print('Applying GNB to', key, '...')
     GNB = GaussianNB()
     GNB.fit(training_data, y_train)
     
-    accuracy_dict_GNB_COUNT[key] = accuracy_score(
+    accuracy_dict_GNB[key] = accuracy_score(
         y_val, GNB.predict(validation_data))
+
 # apply logistic regression with gaussian naive bayes and save the accuracy in logistic regression result dictionary
-def apply_mnb(key, training_data, validation_data):
+def apply_mnb(y_train, y_val, key, training_data, validation_data):
+    global accuracy_dict_MNB
+
     print('Applying MNB to', key, '...')
     MNB = MultinomialNB()
     MNB.fit(training_data, y_train)
     # print ("Accuracy for M. Naive Bayes ngram: %s",    #         % (accuracy_score(y_val, MNB.predict(processed_validation_count))))
-    accuracy_dict_MNB_COUNT[key] = accuracy_score(
+    accuracy_dict_MNB[key] = accuracy_score(
         y_val, MNB.predict(validation_data))
 
 # writes mutual info vocab to file and returns the vocab.
@@ -282,7 +242,7 @@ def mutual_info_feature_selection(stops, X, y, threshold, vectorizer_type):
     print('sort feature scores')
     return high_score_features
     
-def extract_features_with_params(max_features, max_df, min_df, stops):
+def extract_features_with_params(X_train, X_val, max_features, max_df, min_df, stops):
     feature_tfidf_vectorizer = TfidfVectorizer(
         max_features=max_features, max_df=max_df, min_df=min_df, stop_words=stops)
     feature_count_vectorizer = CountVectorizer(
@@ -314,133 +274,142 @@ def write_results(results, result_filenames):
             w = json.dumps(results[i], indent=2)
             f.write(w)    
 
-def test_feature_params_with_model(stops, max_features_list, min_df_list, max_df_list):
+def test_feature_params_with_model(X_train, X_val, y_train, y_val, stops, max_features_list, min_df_list, max_df_list):
     for max_features in max_features_list:
         for max_df in max_df_list:
             for min_df in min_df_list:
                 processed_training_tfidf, processed_validation_tfidf, processed_training_count, processed_validation_count = extract_features_with_params(
-                    max_features, max_df, min_df, stops)
+                    X_train, X_val, max_features, max_df, min_df, stops)
                 key = str(max_df) + ' ' + str(min_df) + ' ' + str(max_features)
 
-                apply_models(key, processed_training_count, processed_validation_count,
+                apply_models(y_train, y_val, key, processed_training_count, processed_validation_count,
                              processed_training_tfidf, processed_validation_tfidf)
 
+def vectorize_data_tfidf_and_count(tfidf_vectorizer, count_vectorizer, X):
+    tfidf_data = tfidf_vectorizer.fit_transform(
+            X).toarray()
+    count_data = count_vectorizer.fit_transform(
+            X).toarray()
+
+    return tfidf_data, count_data
+
+def select_mutual_info_vocab(X_train, y_train, stops, vectorizer_type, max_features, tfidf_filename):
+    selected_vocab = {}
+    try:
+        with open(tfidf_filename, 'r') as f:
+            s = f.read()
+            selected_vocab = eval(s)
+    except:
+        print(tfidf_filename, 'is not found. Creating vocab file:', tfidf_filename)
+        selected_vocab = write_and_get_mutual_info_vocab(
+            max_features, vectorizer_type, stops, X_train, y_train)
+    return selected_vocab
+
+def test_feature_params_with_model_mutual_info(X_train, X_val, y_train, y_val, stops, max_features_list):
+    for max_features in max_features_list:
+        # get mutual info vocab from file if the file is created for COUNT.
+        # else, create the vocab and save it for future use.
+        count_filename = 'mi_vocab/count' + str(max_features) + '.txt'
+        selected_vocab_count =  select_mutual_info_vocab(X_train, y_train, stops, 'count', max_features, count_filename)
+        # get mutual info vocab from file if the file is created for TFIDF.,        # else, create the vocab and save it for future use.
+        tfidf_filename = 'mi_vocab/tfidf' + str(max_features) + '.txt'
+        selected_vocab_tfidf = select_mutual_info_vocab(X_train, y_train, stops, 'tfidf', max_features, tfidf_filename)
+       
+        # vectorize X data according to tf-idf and count
+        feature_tfidf_vectorizer = TfidfVectorizer(
+            vocabulary=selected_vocab_tfidf, stop_words=stops)
+        feature_count_vectorizer = CountVectorizer(
+            vocabulary=selected_vocab_count, stop_words=stops)
+
+        # create vectorized input data
+        processed_training_tfidf, processed_training_count = vectorize_data_tfidf_and_count(feature_tfidf_vectorizer, feature_count_vectorizer, X_train)
+        processed_validation_tfidf, processed_validation_count = vectorize_data_tfidf_and_count(feature_tfidf_vectorizer, feature_count_vectorizer, X_val)
+        
+        # Apply models
+        key = 'mutual info ' + str(max_features)
+        apply_models(y_train, y_val, key, processed_training_count, processed_validation_count,
+                     processed_training_tfidf, processed_validation_tfidf)
+
+def initialize_data(directory_name, file_name_X, file_name_y, X, y):
+    try:
+        X = read_from_txt(file_name_X)
+        y = read_from_txt(file_name_y)
+    except:
+        # extract the training data
+        training_data = extract_data(directory_name)
+        # lemmatize the data
+        format_data(training_data)
+        
+        for data in training_data:
+            X.append(data.content)
+            y.append(data.rating)
+
+        write_to_txt(file_name_X, X)
+        write_to_txt(file_name_y, y)
+    
+    return X, y
+
 if __name__ == "__main__":
-    args = sys.argv
-    
-    #folder_name = args[2]
-    start = time.time()
-    # extract the training data
-    training_data = extract_data('TRAIN')
-    # extract the validation data
-    validation_data = extract_data('VAL')
-    # lemmatize the data
-    format_data(training_data)
-    format_data(validation_data)
+    global accuracy_dict_GNB_COUNT, accuracy_dict_GNB_TFIDF, accuracy_dict_MNB_COUNT, accuracy_dict_MNB_TFIDF, accuracy_dict_LR_COUNT, accuracy_dict_LR_TFIDF, accuracy_dict_RFR_COUNT, accuracy_dict_RFR_TFIDF
 
-    for data in training_data:
-        X_train.append(data.content)
-        y_train.append(data.rating)
-    for data in validation_data:
-        X_val.append(data.content)
-        y_val.append(data.rating)
+    training_data = []
+    validation_data = []
 
-    print('Duration:', time.time() - start)
+    accuracy = 0
+    precision = 0
+    recall = 0
 
-    write_to_txt('x_train.txt', X_train)
-    write_to_txt('y_train.txt', y_train)
-    write_to_txt('x_val.txt', X_val)
-    write_to_txt('y_val.txt', y_val)
+    macro_avg = 0
+
+    X_train = []
+    y_train = []
+    X_val = []
+    y_val = []
+
+    WNL = WordNetLemmatizer()
     
-    X_train = read_from_txt('x_train.txt')
-    y_train = read_from_txt('y_train.txt')
-    X_val = read_from_txt('x_val.txt')
-    y_val = read_from_txt('y_val.txt')
+    print('Initialization begin!')
+    # training data
+    X_train, y_train =  initialize_data('TRAIN', 'x_train.txt', 'y_train.txt', X_train, y_train)
+     # validation data
+    X_val, y_val = initialize_data('VAL', 'x_val.txt', 'y_val.txt', X_val, y_val)
+    print('Initialization done!')
     
+    # initialize stop words
     stops = stopwords.words('english')
-    # To add more stop words, add = symbol to the beginning of the words you want to add in words.txt
+    # To add more stop words, add = symbol to the beginning of the words you want to add in a file called words.txt
     with open('words.txt', 'r') as f:
         lines = f.readlines()
         for line in lines:
             if line.startswith('='):
                 stops.append(line[1:].strip())
 
-    max_features_list = range(10, 760, 10)
-    min_df_list = range(1, 10)
-    max_df_list = [x * 0.1 for x in range(1, 11)]
+    max_features_list = range(50, 60, 10)
+    min_df_list = range(1, 2)
+    max_df_list = [x * 0.1 for x in range(5, 6)]
 
     # create models according to feature extraction by tf/idf
-    test_feature_params_with_model(stops, max_features_list, min_df_list, max_df_list)
+    test_feature_params_with_model(X_train, X_val, y_train, y_val, stops, max_features_list, min_df_list, max_df_list)
 
     # create models according to mutual information selection
-    for max_features in max_features_list:
-        selected_vocab_count = {}
-        selected_vocab_tfidf = {}
-        # get mutual info vocab from file if the file is created for COUNT.
-        # else, create the vocab and save it for future use.
-        count_filename = 'mi_vocab/count' + str(max_features) + '.txt'
-        if os.path.exists(count_filename):
-            print(count_filename, 'is found, retrieving vocab...')
-            with open(count_filename, 'r') as f:
-                s = f.read()
-                selected_vocab_count = eval(s)
-        else:
-            print('Creating vocab file:', count_filename)
-            selected_vocab_count = write_and_get_mutual_info_vocab(
-                max_features, 'count', stops, X_train, y_train)
-        # get mutual info vocab from file if the file is created for TFIDF.
-        # else, create the vocab and save it for future use.
-        tfidf_filename = 'mi_vocab/tfidf' + str(max_features) + '.txt'
-        if os.path.exists(tfidf_filename):
-            print(tfidf_filename, 'is found, retrieving vocab...')
-            with open(tfidf_filename, 'r') as f:
-                s = f.read()
-                selected_vocab_tfidf = eval(s)
-        else:
-            print('Creating vocab file:', tfidf_filename)
-            selected_vocab_tfidf = write_and_get_mutual_info_vocab(
-                max_features, 'tfidf', stops, X_train, y_train)
-
-        feature_tfidf_vectorizer = TfidfVectorizer(
-            vocabulary=selected_vocab_tfidf, stop_words=stops)
-        feature_count_vectorizer = CountVectorizer(
-            vocabulary=selected_vocab_count, stop_words=stops)
-
-        processed_training_tfidf = feature_tfidf_vectorizer.fit_transform(
-            X_train).toarray()
-        processed_training_count = feature_count_vectorizer.fit_transform(
-            X_train).toarray()
-
-        processed_validation_tfidf = feature_tfidf_vectorizer.fit_transform(
-            X_val).toarray()
-        processed_validation_count = feature_count_vectorizer.fit_transform(
-            X_val).toarray()
-        # TODO(eridincu): Remind this link: https://analyticsindiamag.com/7-types-classification-algorithms/
-        key = 'mutual info ' + str(max_features)
-        apply_models(key, processed_training_count, processed_validation_count,
-                     processed_training_tfidf, processed_validation_tfidf)
-    # sort results
-    accuracy_dict_GNB_TFIDF = sorted(
-        accuracy_dict_GNB_TFIDF.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_GNB_COUNT = sorted(
-        accuracy_dict_GNB_COUNT.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_MNB_TFIDF = sorted(
-        accuracy_dict_MNB_TFIDF.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_MNB_COUNT = sorted(
-        accuracy_dict_MNB_COUNT.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_LR_TFIDF = sorted(
-        accuracy_dict_LR_TFIDF.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_LR_COUNT = sorted(
-        accuracy_dict_LR_COUNT.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_RFR_TFIDF = sorted(
-        accuracy_dict_RFR_TFIDF.items(), key=lambda x: x[1], reverse=True)
-    accuracy_dict_RFR_COUNT = sorted(
-        accuracy_dict_RFR_COUNT.items(), key=lambda x: x[1], reverse=True)
-
-    accuracy_dict_list = [accuracy_dict_GNB_TFIDF, accuracy_dict_GNB_COUNT, accuracy_dict_MNB_TFIDF,
-         accuracy_dict_MNB_COUNT, accuracy_dict_LR_TFIDF, accuracy_dict_LR_COUNT, accuracy_dict_RFR_TFIDF, accuracy_dict_RFR_COUNT]
-    accuracy_dict_list_names = ['result_accuracy_dict_GNB_TFIDF', 'result_accuracy_dict_GNB_COUNT', 'result_accuracy_dict_MNB_TFIDF',
-               'result_accuracy_dict_MNB_COUNT', 'result_accuracy_dict_LR_TFIDF', 'result_accuracy_dict_LR_COUNT', 'result_accuracy_dict_RFR_TFIDF', 'result_accuracy_dict_RFR_COUNT']
+    test_feature_params_with_model_mutual_info(X_train, X_val, y_train, y_val, stops, max_features_list)
+    
+    # Sort results and write them to file
+    accuracy_dict_GNB = sorted(
+        accuracy_dict_GNB.items(), key=lambda x: x[1], reverse=True)
+    
+    accuracy_dict_MNB = sorted(
+        accuracy_dict_MNB.items(), key=lambda x: x[1], reverse=True)
+    
+    accuracy_dict_LR = sorted(
+        accuracy_dict_LR.items(), key=lambda x: x[1], reverse=True)
+    
+    accuracy_dict_RFR = sorted(
+        accuracy_dict_RFR.items(), key=lambda x: x[1], reverse=True)
+    
+    # prepare the data, and print the results to a file
+    accuracy_dict_list = [accuracy_dict_GNB, accuracy_dict_MNB, accuracy_dict_LR, accuracy_dict_RFR]
+    accuracy_dict_list_names = ['result_accuracy_dict_GNB', 'result_accuracy_dict_MNB', 'result_accuracy_dict_LR', 'result_accuracy_dict_RFR']
 
     write_results(accuracy_dict_list, accuracy_dict_list_names)
 
