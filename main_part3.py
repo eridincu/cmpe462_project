1b7712d80784eb4cc8089a113df5b2397701877c
oyku <oykuyilmaz.228@gmail.com>
2021-06-08 18:10:07 +0300

    logistic regression added

--- main_part3.py
+++ main_part3.py
@@ -27,12 +27,14 @@ from sklearn.feature_selection import mutual_info_classif
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 
 accuracy_dict_MNB = {}
 accuracy_dict_GNB = {}
-accuracy_dict_LR = {}
+accuracy_dict_LOG_REG = {}
+accuracy_dict_LIN_REG = {}
 accuracy_dict_RFR = {}
 
 class Data():
@@ -150,8 +152,8 @@ def apply_models(y_train, y_val, key, processed_training_count, processed_valida
     apply_gnb(y_train, y_val, 'count ' + key, processed_training_count, processed_validation_count)
     apply_gnb(y_train, y_val, 'tfidf ' + key, processed_training_tfidf, processed_validation_tfidf)
 
-    apply_lr_cross_val(y_train, y_val, 'count ' + key, processed_training_count, processed_validation_count)
-    apply_lr_cross_val(y_train, y_val, 'tfidf ' + key, processed_training_tfidf, processed_validation_tfidf)
+    apply_log_reg(y_train, y_val, 'count ' + key, processed_training_count, processed_validation_count)
+    apply_log_reg(y_train, y_val, 'tfidf ' + key, processed_training_tfidf, processed_validation_tfidf)
 
     print('Done, exec time:', time.time() - start)
 
@@ -178,15 +180,38 @@ def apply_rfr(y_train, y_val, key, processed_training_count, processed_validatio
     accuracy_dict_RFR[str(n_estimator) + '_' + key] = score
 
 # apply logistic regression with 5-fold cross validation and save the accuracy in logistic regression result dictionary
-def apply_lr_cross_val(y_train, y_val, key, training_data, validation_data):
-    global accuracy_dict_LR
+def apply_log_reg(y_train, y_val, key, training_data, validation_data):
+    global accuracy_dict_LOG_REG
 
     print('Applying LRC to', key, '...')
-    LR = LogisticRegressionCV(cv=5, max_iter=100000, n_jobs=-1)
+    LR = LogisticRegression(max_iter=100000, n_jobs=-1)
+    # LR = LogisticRegressionCV(cv=5, max_iter=100000, n_jobs=-1)
     LR.fit(training_data, y_train)
 
     score = LR.score(validation_data, y_val)
-    accuracy_dict_LR[key] = score
+    accuracy_dict_LOG_REG[key] = score
+
+def apply_lin_reg(y_train, y_val, key, training_data, validation_data):
+    global accuracy_dict_LIN_REG
+    y_train_linreg = np.copy(y_train)
+    y_val_linreg = np.copy(y_val)
+
+    y_train_linreg[y_train_linreg == 'P'] = 3
+    y_train_linreg[y_train_linreg == 'Z'] = 2
+    y_train_linreg[y_train_linreg == 'N'] = 1
+    y_val_linreg[y_val_linreg == 'P'] = 3
+    y_val_linreg[y_val_linreg == 'Z'] = 2
+    y_val_linreg[y_val_linreg == 'N'] = 1
+
+    y_train_linreg = y_train_linreg.astype('float64')
+    y_val_linreg = y_val_linreg.astype('float64')
+    print('Applying LRC to', key, '...')
+    LR = LinearRegression(n_jobs=-1)
+    # LR = LogisticRegressionCV(cv=5, max_iter=100000, n_jobs=-1)
+    LR.fit(training_data, y_train_linreg)
+
+    score = LR.score(validation_data, y_val_linreg)
+    accuracy_dict_LIN_REG[key] = score
 
 # apply logistic regression with gaussian naive bayes and save the accuracy in logistic regression result dictionary
 def apply_gnb(y_train, y_val, key, training_data, validation_data):
@@ -247,7 +272,7 @@ def extract_features_with_params(X_train, X_val, max_features, max_df, min_df, s
 
 def write_results(results, result_filenames):
     for i in range(len(results)):
-        with open('results/' + result_filenames[i] + '.json', 'w') as f:
+        with open('resultsGlove/' + result_filenames[i] + '.json', 'w') as f:
             w = json.dumps(results[i], indent=2)
             f.write(w)    
 
@@ -279,6 +304,29 @@ def initialize_data(directory_name, file_name_X, file_name_y, X, y,stops):
     
     return X, y
 
+def getGloveVectors(total_vocabulary):  
+    start = time.time()  
+    print("getGloveVectors started")   
+    glove = {}
+    with open('glove.840B.300d.txt', 'rb') as f:
+        for line in f:
+            parts = line.split()
+            word = parts[0].decode('utf-8')
+            if word in total_vocabulary:
+                vector = np.array(parts[1:], dtype=np.float32)
+                glove[word] = vector
+    dim = len(glove[next(iter(glove))])
+    print("getGloveVectors finished in time ",time.time()-start)
+
+    return glove, dim
+
+def gloveVectorizer(glove, X, dimension):
+    start = time.time()  
+    print("gloveVectorizer started")   
+    X_vector = np.array([np.mean([glove[w] for w in words if w in glove] or [np.zeros(dimension)], axis=0) for words in X])
+    print("gloveVectorizer finished in time ",time.time()-start)
+    return X_vector
+
 if __name__ == "__main__":
     training_data = []
     validation_data = []
@@ -304,28 +352,32 @@ if __name__ == "__main__":
      # validation data
     X_val, y_val = initialize_data('VAL', 'x_val.txt', 'y_val.txt', X_val, y_val, stops)
     print('Initialization done!')
-    print(X_val[0])
-    exit()
+    total_vocabulary = set(word for document in X_train for word in document)
+    gloveDict, dim = getGloveVectors(total_vocabulary)
+    X_train_vector = gloveVectorizer(gloveDict, X_train, dim)
+    X_val_vector = gloveVectorizer(gloveDict, X_val, dim)
+    apply_log_reg(y_train, y_val, "glove" , X_train_vector, X_val_vector)
 
+    
 
     # create models according to feature extraction by tf/idf
-    test_feature_params_with_model(X_train, X_val, y_train, y_val, stops)
+    # test_feature_params_with_model(X_train, X_val, y_train, y_val, stops)
     
     # Sort results and write them to file
-    accuracy_dict_GNB = sorted(
-        accuracy_dict_GNB.items(), key=lambda x: x[1], reverse=True)
+    # accuracy_dict_GNB = sorted(
+    #     accuracy_dict_GNB.items(), key=lambda x: x[1], reverse=True)
     
-    accuracy_dict_MNB = sorted(
-        accuracy_dict_MNB.items(), key=lambda x: x[1], reverse=True)
+    # accuracy_dict_MNB = sorted(
+    #     accuracy_dict_MNB.items(), key=lambda x: x[1], reverse=True)
     
-    accuracy_dict_LR = sorted(
-        accuracy_dict_LR.items(), key=lambda x: x[1], reverse=True)
+    # accuracy_dict_LOG_REG = sorted(
+    #     accuracy_dict_LOG_REG.items(), key=lambda x: x[1], reverse=True)
     
-    accuracy_dict_RFR = sorted(
-        accuracy_dict_RFR.items(), key=lambda x: x[1], reverse=True)
+    # accuracy_dict_RFR = sorted(
+    #     accuracy_dict_RFR.items(), key=lambda x: x[1], reverse=True)
     
     # prepare the data, and print the results to a file
-    accuracy_dict_list = [accuracy_dict_GNB, accuracy_dict_MNB, accuracy_dict_LR, accuracy_dict_RFR]
-    accuracy_dict_list_names = ['result_accuracy_dict_GNB', 'result_accuracy_dict_MNB', 'result_accuracy_dict_LR', 'result_accuracy_dict_RFR']
+    accuracy_dict_list = [accuracy_dict_GNB, accuracy_dict_MNB, accuracy_dict_LOG_REG, accuracy_dict_RFR, accuracy_dict_LIN_REG]
+    accuracy_dict_list_names = ['result_accuracy_dict_GNB', 'result_accuracy_dict_MNB', 'result_accuracy_dict_LOG_REG', 'result_accuracy_dict_RFR','accuracy_dict_LIN_REG']
 
     write_results(accuracy_dict_list, accuracy_dict_list_names)
